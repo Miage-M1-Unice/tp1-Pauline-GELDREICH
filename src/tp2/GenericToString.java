48abab7865bef1524de0e465ac43907872b15484
paulinegld <pauline.geldreich@etu.unice.fr>
2019-02-28 15:01:20 +0100

    toString avec profondeur

--- src/tp2/GenericToString.java
+++ src/tp2/GenericToString.java
@@ -9,22 +9,60 @@ import java.util.Arrays;
 
 public class GenericToString {
 
-    public String toString(Object object) {
+
+
+    public String toString(Object object,int profondeur) throws IllegalAccessException {
        Class cl = object.getClass();
         String nomClasse = cl.getName();
         String res = "";
 
-        Field[] fields = cl.getFields();
-        for (int i = 0; i < fields.length; i++) {
-            res += fields[i].getName() ;
-            if (i < fields.length - 1) {
+        ArrayList<Field> fields = fieldsEnProfondeur(cl);
+
+        for (int i = 0; i < fields.size(); i++) {
+
+            if (profondeur <= 0 ) return "" ;
+
+            fields.get(i).setAccessible(true);
+
+
+            if(fields.get(i).getType().isPrimitive()){
+
+                res += fields.get(i).getName();
+                res += "= " + fields.get(i).get(object) ;
+
+            }else if(fields.get(i).getType().isArray()){
+
+                res += fields.get(i).getName();
+                res += "={";
+
+                for (int j = 0; j < Array.getLength(fields.get(i).get(object)); j++) {
+                    res += Array.get(fields.get(i).get(object), j);
+
+                    if(j != Array.getLength(fields.get(i).get(object))-1)
+                        res += ",";
+                }
+                res += "} ";
+            }else{
+                res += toString(fields.get(i).get(object), profondeur - 1)+"\n";
+            }
+            if (i < fields.size() - 1) {
                 res += "; ";
             }
         }
         return nomClasse +"[" + res + "]";
     }
 
+    public ArrayList<Field> fieldsEnProfondeur(Class cl) {
+        ArrayList<Field> fields = new ArrayList<>(Arrays.asList(cl.getFields()));
+        for (Field field : cl.getDeclaredFields()) {
+            if(!fields.contains(field))
+                fields.add(field);
+        }
+        return fields;
+    }
+
 }
+
 
 
 
--- src/tp2/Main.java
+++ src/tp2/Main.java
@@ -5,16 +5,16 @@ import java.awt.*;
 
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException{
+    public static void main(String[] args) throws IllegalAccessException {
 
         String name ="java.awt.Point";
         //InfoParIntrospection.analyseClasse(name);
 
-        System.out.println(new GenericToString().toString(new Point(12,24)));
+        System.out.println(new GenericToString().toString(new Point(12,24), 1) );
 
         Polygon pol = new Polygon(new int[]{10, 20, 30}, new int[]{20,30, 40}, 3);
         pol.getBounds();
 
-        System.out.println(new GenericToString().toString(pol));
+        System.out.println(new GenericToString().toString(pol, 2));
     }
 }
